d6865ef59987daa94926c17b3ba26075c6834462
RashingPro <136486976+RashingPro@users.noreply.github.com>
2026-06-16 21:57:25 +0300

    perf(cli): optimize `main.rs`

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,47 +1,17 @@
-use crate::help::print_help;
-use backend::Evaluator;
-use std::{env::args, path::Path, rc::Rc};
-use ultraviolet_core::{
-    errors::SpannedError,
-    types::{
-        backend::{ControlFlow, UVRTValue},
-        frontend::{SourceFile, number::Number},
-    },
-};
-mod help;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = args().collect();
-
-    let path = match args.get(1) {
-        Some(path) => path,
-        None => print_help(),
-    };
-
-    let source = match SourceFile::load(Path::new(path)) {
-        Ok(s) => Rc::new(s),
-        Err(err) => {
-            eprintln!("Can't open source file: {}", err);
-            std::process::exit(-1);
-        },
-    };
-
-    let ret = run(source).unwrap_or_else(|err| {
-        eprintln!("{err}");
-        ControlFlow::Simple(UVRTValue::Number(Number::I8(-1)))
-    });
-
-    let return_code = match ret {
-        ControlFlow::Simple(UVRTValue::Number(Number::I8(v)))
-        | ControlFlow::Return(UVRTValue::Number(Number::I8(v))) => v,
-        _ => 0,
-    };
-
-    std::process::exit(return_code.into());
-}
-
-fn run(source: Rc<SourceFile>) -> Result<ControlFlow, SpannedError> {
-    let sfp = frontend::process_file(source, false)?;
-    let evaluator = Evaluator::new(sfp, "");
-    evaluator.eval()
-}
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣿⣿⣿⣿⡆⠀⠀⠀⠀⢸⣿⣿⣿⣶
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⣿⣿⣿⣇⠀⠀⠀⠀⢰⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⣦⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⡇⠀⠀⠀⠀⢸⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠘⢿⡤⠀⠀⠀⠀⠀⠀⠠⣿⣿⣿⣿⡇⠀⠀⠀⠀⢸⣿⣿⣿⣿
+⠀⠀⢹⣆⣀⣀⣀⣾⣿⣆⣀⡀⠀⠀⠀⢰⣿⣿⣿⣿⡇⠀⠀⠀⠀⢸⣿⣿⣿⣿
+⠀⠀⢼⠋⠉⠉⠉⣻⣿⠛⠉⠈⠁⠀⠀⢘⣿⣿⣿⣿⡏⠀⠀⠀⠀⠘⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⣰⣿⠋⠀⠀⠀⠀⠀⠀⢈⣿⣿⣿⣿⣏⠀⠀⠀⠀⠈⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠏⠁⠀⠀⠀⠀⠀⠀⠀⢈⣿⣿⡿⢿⡗⠀⠀⠀⠀⠀⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣷⣿⡏⠀⠀⠀⠀⢰⣿⣿⣿⡿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
