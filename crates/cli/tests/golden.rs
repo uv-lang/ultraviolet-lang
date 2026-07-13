800c1606ed03ed49e9c641aaa66ac48daa616275
Andcool-Systems <drolll2007@gmail.com>
2026-07-13 21:38:51 +0300

    feat(tests): Enhance golden tests with detailed error reporting and add new function in mod.uv

--- crates/cli/tests/golden.rs
+++ crates/cli/tests/golden.rs
@@ -1,4 +1,5 @@
 use assert_cmd::Command;
+use colored::Colorize;
 use pretty_assertions::assert_eq;
 use std::error::Error;
 use std::fs;
@@ -9,16 +10,32 @@ fn normalize(s: &str) -> String {
     s.replace("\r\n", "\n")
 }
 
+struct TestError {
+    pub actual: String,
+    pub expected: String,
+    pub test_name: String,
+}
+
 #[test]
 fn golden_tests() -> Result<(), Box<dyn Error>> {
     let cases_dir = Path::new("../../tests/cases").canonicalize()?;
 
+    let mut latest_err: Option<TestError> = None;
     for entry in fs::read_dir(cases_dir).unwrap() {
         let path = entry.unwrap().path();
 
         if path.extension().and_then(|s| s.to_str()) != Some("uv") {
             continue;
         }
+
+        let test_name = path
+            .components()
+            .next_back()
+            .unwrap()
+            .as_os_str()
+            .to_string_lossy();
+
+        print!("test golden::{test_name} ... ");
 
         let expected_path = path.with_extension("out");
         let expected = fs::read_to_string(&expected_path)
@@ -31,16 +48,20 @@ fn golden_tests() -> Result<(), Box<dyn Error>> {
             String::from_utf8(output.stderr).unwrap()
         };
 
-        assert_eq!(
-            normalize(&actual),
-            normalize(&expected),
-            "Test `{}` failed",
-            path.components()
-                .next_back()
-                .unwrap()
-                .as_os_str()
-                .to_string_lossy()
-        );
+        if normalize(&actual) != normalize(&expected) {
+            println!("{}", "ERROR".red());
+            latest_err = Some(TestError {
+                actual: normalize(&actual),
+                expected: normalize(&expected),
+                test_name: test_name.to_string(),
+            })
+        } else {
+            println!("{}", "ok".green());
+        }
+    }
+
+    if let Some(err) = latest_err {
+        assert_eq!(err.actual, err.expected, "Test `{}` failed", err.test_name);
     }
 
     Ok(())
